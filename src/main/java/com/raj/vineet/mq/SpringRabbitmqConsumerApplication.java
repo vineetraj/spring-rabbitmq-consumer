1e1ab27a384dd0180efa1ab120bf012107f28e82
Vineet Raj <vineetraj14@gmail.com>
2022-10-30 12:49:48 +0530

    Initial commit

---
+++ src/main/java/com/raj/vineet/mq/SpringRabbitmqConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.raj.vineet.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitmqConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitmqConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raj/vineet/mq/SpringRabbitmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raj.vineet.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
